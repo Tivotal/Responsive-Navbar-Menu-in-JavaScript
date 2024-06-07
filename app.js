c16475cd59b50ed3f3b699fcd344107734fa434b
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-06-07 08:01:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+/* Created by Tivotal */
+
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.right = "0";
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.right = "-100%";
+}
